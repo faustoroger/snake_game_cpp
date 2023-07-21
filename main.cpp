9cead74496290fb076ca946e4949f3121d805df9
Fausto Roger <hixian@gmail.com>
2023-07-21 20:10:50 -0300

    feat: add snake drawing and screen positioning

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
-#include <iostream>  // time e rand
 #include <ncurses.h> // para as funções da ncurses
+#include <iostream>  // time e rand
 #include <vector>    // lógica das coordenadas
 #include <unistd.h>  //usleep()
+
+// Lógica para armazenar as coordenadas
+struct SnakeType
+{
+    int s_x, s_y;
+    SnakeType(int s_col, int s_row)
+    {
+        s_x = s_col;
+        s_y = s_row;
+    }
+    SnakeType()
+    {
+        s_x = 0;
+        s_y = 0;
+    }
+};
 
 class SnakeGame
 {
 protected:
     int m_maxwidth;  // tamanho da largura da tela
     int m_maxheight; // tamanho da altura da tela
+    std::vector<SnakeType> snake;
+    char m_snake_char; // desenha a snake
 
 public:
     SnakeGame()
@@ -21,6 +39,9 @@ public:
         m_maxwidth = getmaxx(stdscr) / 2;
         m_maxheight = getmaxy(stdscr) / 2;
 
+        m_snake_char = 'o'; // desenha a snake
+
+        // Desenha o espaço do game
         for (int i = 0; i < m_maxwidth - 1; ++i)
         {
             move(0, i);
@@ -53,7 +74,7 @@ public:
 
         for (int i = 0; i < m_maxwidth - 1; ++i)
         {
-            move(m_maxwidth - 2, i);
+            move(m_maxheight - 2, i);
             if (i == 0)
             {
                 addch('+');
@@ -83,6 +104,19 @@ public:
             {
                 addch('|');
             }
+        }
+
+        // desenha o tamanho inicial da snake
+        for (int i = 0; i < 5; ++i)
+        {
+            snake.push_back(SnakeType(40 + i, 10));
+        }
+
+        // Lógica para posicionar
+        for (int i = 0; i < snake.size(); ++i)
+        {
+            move(snake[i].s_y, snake[i].s_x);
+            addch(m_snake_char);
         }
     }
 
